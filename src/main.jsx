b29fa1785262f0f579089d765a7eabfe753b5bf6
bibiamina <ahnafnihal0999@gmail.com>
2024-10-26 22:05:45 +0600

    navbar added

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
   },
 ]);
 
